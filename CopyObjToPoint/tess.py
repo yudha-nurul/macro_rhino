715fb8e74078fb8ee77b2f0aebdedc7961f4c83b
Yudha Nurul Alfian <67511620+yudha-nurul@users.noreply.github.com>
2023-02-26 00:52:19 +0700

    Update tess.py

--- CopyObjToPoint/tess.py
+++ CopyObjToPoint/tess.py
@@ -15,3 +15,12 @@ print(x)
             #translation = point-start
             #rs.CopyObjects( objectIds, translation )
             
+"""
+listnya = ['10.000 34.000 50.000', '3.000 5.000 9.000']
+print(listnya)
+co = ('_Copy 0 0 0 ')
+jadi = ' '.join(listnya)
+print(jadi)
+jadi2 = ('"' + co + jadi + '"')
+print(jadi2)
+"""
